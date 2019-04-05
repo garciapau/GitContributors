304f6e0cc6d2255a59da0f1ddcc218325e4b2e16
pau-garcia <pau.garciazorrilla@clarivate.com>
2019-04-05 20:54:46 +0200

    add first empty test getContributors

---
+++ infra/rest/src/test/java/com/acme/git/contributors/infra/rest/controller/GitContributorsControllerTest.java
@@ -0,0 +1,10 @@
+package com.acme.git.contributors.infra.rest.controller;
+
+import org.junit.Test;
+
+public class GitContributorsControllerTest {
+    @Test
+    public void getContributors()
+    {
+    }
+}
