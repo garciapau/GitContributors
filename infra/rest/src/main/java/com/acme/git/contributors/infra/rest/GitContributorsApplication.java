cca2a68f33602dcb9748c77c514170f3f1408f16
pau-garcia <pau.garciazorrilla@clarivate.com>
2019-04-05 20:30:10 +0200

    initialize gradle and create a spring rest module for HTTP port

---
+++ infra/rest/src/main/java/com/acme/git/contributors/infra/rest/GitContributorsApplication.java
@@ -0,0 +1,13 @@
+package com.acme.git.contributors.infra.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitContributorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitContributorsApplication.class, args);
+    }
+
+}
