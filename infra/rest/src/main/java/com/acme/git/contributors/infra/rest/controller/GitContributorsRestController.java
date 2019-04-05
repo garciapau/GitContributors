b6b5be6468a40c61c081cbcdec4729bc5ee4c0f2
pau-garcia <pau.garciazorrilla@clarivate.com>
2019-04-05 22:08:16 +0200

    configure GET for resource contributors

--- infra/rest/src/main/java/com/acme/git/contributors/infra/rest/controller/GitContributorsRestController.java
+++ infra/rest/src/main/java/com/acme/git/contributors/infra/rest/controller/GitContributorsRestController.java
@@ -1,10 +1,12 @@
 package com.acme.git.contributors.infra.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitContributorsRestController implements GitContributorsController {
     @Override
+    @GetMapping(value = "/contributors")
     public void getContributors() {
 
     }
