ab882c18a0bd425c1fee767d91033fa1b3acfb88
pau-garcia <pau.garciazorrilla@clarivate.com>
2019-04-06 19:13:05 +0200

    mocking the remote service inside cucumber feature

--- application/src/test/java/com/acme/git/contributors/application/feature/GetContributorsStepdefs.java
+++ application/src/test/java/com/acme/git/contributors/application/feature/GetContributorsStepdefs.java
@@ -1,14 +1,34 @@
 package com.acme.git.contributors.application.feature;
 
+import com.acme.git.contributors.remote.GitService;
 import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 public class GetContributorsStepdefs implements En {
+    private GitService gitService;
+    private ObtainContributorsByCity obtainContributorsByCity;
+    private List<Object> contributors;
+
     public GetContributorsStepdefs() {
-        Given("^a Github service API$", () -> {
+        Before(() ->{
+            gitService = Mockito.mock(GitService.class);
+            Mockito.when(gitService.getContributorsByCity(any(String.class))).thenReturn(new ArrayList<>());
         });
-        When("^user requests contributors of city 'Barcelona'$", () -> {
+
+        Given("^a Github service API$", () -> {
+            obtainContributorsByCity = new ObtainContributorsByCity(gitService);
+        });
+        When("^user requests contributors of city '(.*)'$", (String city) -> {
+            contributors = obtainContributorsByCity.getContributors(city);
         });
         Then("^a list of contributors is returned$", () -> {
+            Assert.assertNotNull(contributors);
         });
     }
 }
